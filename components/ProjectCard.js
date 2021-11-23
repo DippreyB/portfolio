9fd62ea0adf2dc7c3fa6c52917aae2b91260e67c
DippreyB <48030605+DippreyB@users.noreply.github.com>
2021-11-22 21:27:46 -0600

    portfolio complete

---
+++ components/ProjectCard.js
@@ -0,0 +1,25 @@
+import Image from 'next/image'
+import styles from '../styles/Project.module.css'
+import { useState } from 'react'
+import {FiExternalLink} from 'react-icons/fi'
+
+export default function ProjectCard({title, link, children}) {
+    const [showDetails, setShowDetails] = useState()
+    return (
+        <>
+            
+                <div className={styles.card}>
+                    
+                    <h1>
+                        {title}
+                        <a href={link} target='_blank'><FiExternalLink/></a>
+                    </h1>
+                    
+                    <>{children}</>
+                </div>
+            
+            
+        
+        </>
+    )
+}
